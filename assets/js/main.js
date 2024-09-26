cce535c75bfa9a1aafb464f805f6ae82cd64a8f7
goncalvs98 <goncgbr@gmail.com>
2024-09-26 14:10:19 -0300

    StockFlux Dashboard Ajustes para apresentação dos gráficos

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,47 +88,48 @@ async function populateGrid(table, params, tbody) {
     }
 }
 
-// // Exemplo de gráfico usando Chart.js
-// var ctx1 = document.getElementById('chart1').getContext('2d');
-// var chart1 = new Chart(ctx1, {
-//     type: 'bar',
-//     data: {
-//         labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
-//         datasets: [{
-//             label: 'Métricas 1',
-//             data: [12, 19, 3, 5, 2, 3],
-//             backgroundColor: 'rgba(75, 192, 192, 0.2)',
-//             borderColor: 'rgba(75, 192, 192, 1)',
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             y: {
-//                 beginAtZero: true
-//             }
-//         }
-//     }
-// });
+window.onload = function() {
+    var ctx1 = document.getElementById('chart1').getContext('2d');
+    var chart1 = new Chart(ctx1, {
+        type: 'bar',
+        data: {
+            labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
+            datasets: [{
+                label: 'Amoxicilina',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 
-// var ctx2 = document.getElementById('chart2').getContext('2d');
-// var chart2 = new Chart(ctx2, {
-//     type: 'line',
-//     data: {
-//         labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
-//         datasets: [{
-//             label: 'Métricas 2',
-//             data: [3, 2, 2, 6, 7, 8],
-//             backgroundColor: 'rgba(153, 102, 255, 0.2)',
-//             borderColor: 'rgba(153, 102, 255, 1)',
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             y: {
-//                 beginAtZero: true
-//             }
-//         }
-//     }
-// });
+    var ctx2 = document.getElementById('chart2').getContext('2d');
+    var chart2 = new Chart(ctx2, {
+        type: 'line',
+        data: {
+            labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
+            datasets: [{
+                label: 'Ibuprofeno',
+                data: [3, 2, 2, 6, 7, 8],
+                backgroundColor: 'rgba(153, 102, 255, 0.2)',
+                borderColor: 'rgba(153, 102, 255, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+    };
